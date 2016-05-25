3581ba4b4749ab7c150a119e479f3aa8dff7bade
iamkyleee <hackintosh@Hackintoshs-iMac.local>
2016-05-25 05:05:36 -0700

    result to results

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ controller.on('message_received', function(bot, message) {
       // bot.reply(message, "Your Coords: " + lat + ", "+ long);
       httpRequest('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+ lat +','+ long +'&radius=500&type='+type+'&key=AIzaSyBEDsria02odnrGQPz2Gj_MS_RwdoeG9rw', function(error, response, body){
 
-        var hospitals = body.result
+        var hospitals = body.results
 
         if (!error && response.statusCode == 200) {
           bot.reply(message, {
