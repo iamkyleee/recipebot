6b1ad4e94112d4046aa9815f74d49238cdcde932
iamkyleee <hackintosh@Hackintoshs-iMac.local>
2016-05-25 00:49:38 -0700

    quoted hospital

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ controller.on('message_received', function(bot, message) {
       lat = location.lat;
       long = location.long
 
-      var hospitals = findPlaces(lat, long, hospital)
+      var hospitals = findPlaces(lat, long, 'hospital')
       bot.reply(message, "Your Coords: " + lat + ", "+ long);
       bot.reply(message, hospitals);
       bot.reply(message, {
