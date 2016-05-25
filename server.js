a1f59d0b5d4b4618de3db98ef8f21a437d730b95
iamkyleee <hackintosh@Hackintoshs-iMac.local>
2016-05-25 08:57:16 -0700

    long to lng

--- server.js
+++ server.js
@@ -117,7 +117,6 @@ controller.on('message_received', function(bot, message) {
             return;
         }
 
-        console.log("LAT SAMPLE: ", hostpitals.results[0].geometry.location.lat());
 
         // console.log("FIRST HOSPITAL: ", body.results[0].name);
 
@@ -137,7 +136,7 @@ controller.on('message_received', function(bot, message) {
                 "buttons":[
                   {
                     "type":"web_url",
-                    "url":"https://www.google.com/maps/dir/Current+Location/"+hospitals.results[0].geometry.location.lat() +","+ hospitals.results[0].geometry.location.long() +"",
+                    "url":"https://www.google.com/maps/dir/Current+Location/"+hospitals.results[0].geometry.location.lat +","+ hospitals.results[0].geometry.location.lng +"",
                     "title":"Get Directions"
                   },
                   {
@@ -159,7 +158,7 @@ controller.on('message_received', function(bot, message) {
                 "buttons":[
                   {
                     "type":"web_url",
-                    "url":"https://www.google.com/maps/dir/Current+Location/"+hospitals.results[1].geometry.location.lat() +","+ hospitals.results[1].geometry.location.long() +"",
+                    "url":"https://www.google.com/maps/dir/Current+Location/"+hospitals.results[1].geometry.location.lat +","+ hospitals.results[1].geometry.location.lng +"",
                     "title":"Get Directions"
                   },
                   {
